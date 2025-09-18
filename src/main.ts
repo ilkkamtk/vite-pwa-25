876f730b18aed6c3d6dc06d91422edddc9bae7ca
ilkkamtk <ilkka.kylmaniemi@metropolia.fi>
2025-09-18 14:52:57 +0300

    feat: add PWA support with manifest and service worker
    style: create main CSS file with custom styles and fonts
    
    feat: implement fetchData function for API calls
    
    feat: define interfaces for user data handling
    
    feat: add main application logic for user authentication and profile management
    
    chore: add TypeScript configuration and environment definitions
    
    chore: update Vite configuration for project setup

---
+++ src/main.ts
@@ -0,0 +1,132 @@
+import {fetchData} from './functions';
+import {UpdateResult} from './interfaces/UpdateResult';
+import {UploadResult} from './interfaces/UploadResult';
+import {LoginUser, UpdateUser, User} from './interfaces/User';
+import {apiUrl, uploadUrl} from './variables';
+
+// PWA code
+
+// select forms from the DOM
+const loginForm = document.querySelector(
+  '#login-form'
+) as HTMLFormElement | null;
+const profileForm = document.querySelector(
+  '#profile-form'
+) as HTMLFormElement | null;
+const avatarForm = document.querySelector(
+  '#avatar-form'
+) as HTMLFormElement | null;
+
+// select inputs from the DOM
+const usernameInput = document.querySelector(
+  '#username'
+) as HTMLInputElement | null;
+const passwordInput = document.querySelector(
+  '#password'
+) as HTMLInputElement | null;
+
+const profileUsernameInput = document.querySelector(
+  '#profile-username'
+) as HTMLInputElement | null;
+const profileEmailInput = document.querySelector(
+  '#profile-email'
+) as HTMLInputElement | null;
+
+const avatarInput = document.querySelector(
+  '#avatar'
+) as HTMLInputElement | null;
+
+// select profile elements from the DOM
+const usernameTarget = document.querySelector(
+  '#username-target'
+) as HTMLSpanElement | null;
+const emailTarget = document.querySelector(
+  '#email-target'
+) as HTMLSpanElement | null;
+const avatarTarget = document.querySelector(
+  '#avatar-target'
+) as HTMLImageElement | null;
+
+// TODO: function to login
+const login = async (): Promise<LoginUser> => {
+  if (!usernameInput || !passwordInput) {
+    throw new Error('käyttäjä tai salasana kenttää ei ole');
+  }
+  const username = usernameInput.value;
+  const password = passwordInput.value;
+
+  const data = {
+    username,
+    password,
+  };
+
+  const options: RequestInit = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  };
+
+  const loginResult = await fetchData<LoginUser>(
+    apiUrl + '/auth/login',
+    options
+  );
+  return loginResult;
+};
+
+// TODO: function to update user data
+const updateUserData = async (
+  user: UpdateUser,
+  token: string
+): Promise<UpdateResult> => {};
+
+// TODO: function to add userdata (email, username and avatar image) to the
+// Profile DOM and Edit Profile Form
+const addUserDataToDom = (user: User): void => {
+  if (!usernameTarget || !emailTarget || !avatarTarget) {
+    throw new Error('kohde-elementit puuttuu');
+  }
+  usernameTarget.innerText = user.username;
+  emailTarget.innerText = user.email;
+  avatarTarget.src = uploadUrl + user.avatar;
+};
+
+// function to get userdata from API using token
+const getUserData = async (token: string): Promise<User> => {};
+
+// TODO: function to check local storage for token and if it exists fetch
+// userdata with getUserData then update the DOM with addUserDataToDom
+const checkToken = async (): Promise<void> => {};
+
+// call checkToken on page load to check if token exists and update the DOM
+checkToken();
+
+// TODO: login form event listener
+// event listener should call login function and save token to local storage
+// then call addUserDataToDom to update the DOM with the user data
+if (!loginForm) {
+  console.error('login lomake puuttuu');
+} else {
+  loginForm.addEventListener('submit', async (evt) => {
+    try {
+      evt.preventDefault();
+      const loginResult = await login();
+      console.log(loginResult);
+      localStorage.setItem('token', loginResult.token);
+      addUserDataToDom(loginResult.data);
+    } catch (error) {
+      alert((error as Error).message);
+    }
+  });
+}
+
+// TODO: profile form event listener
+// event listener should call updateUserData function and update the DOM with
+// the user data by calling addUserDataToDom or checkToken
+profileForm?.addEventListener('submit', () => {});
+
+// TODO: avatar form event listener
+// event listener should call uploadAvatar function and update the DOM with
+// the user data by calling addUserDataToDom or checkToken
+avatarForm?.addEventListener('submit', () => {});
